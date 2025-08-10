83fecd97ae31067d2ceab75b361e9f516145352f
Leonard Püttmann <leopuettmann@tuta.io>
2025-08-10 20:02:08 +0200

    Added module on distributed indexing, started adding tasks to the modules

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='information_retrieval_course',
+    version='0.1',
+    packages=find_packages(),
+)
--- util/load_lotr.py
+++ util/load_lotr.py
@@ -5,7 +5,7 @@ def lotr_chapters():
     project_dir = script_dir.parent
     file_path = project_dir / "lotr.txt"
 
-    with open(file_path, "r") as file:
+    with open(file_path, "r", encoding="ISO-8859-1") as file:
         text = file.read()
 
     chapters = text.split("\n\n")
